1310205af5a9be43e580bf5120d0c153387878ec
devude7 <dawidmrowczynski7@gmail.com>
2025-02-03 19:53:10 +0100

    result print and layers update

--- iris_multi.py
+++ iris_multi.py
@@ -16,7 +16,7 @@ y = np.eye(3)[y]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-MLP = MultiPerceptron([4,3,3], lr=0.01, epochs=1000)
+MLP = MultiPerceptron([4,16,8,3], lr=0.01, epochs=100)
 MLP.fit(X_train, y_train)
 
 predictions = MLP.predict(X_test)
@@ -24,5 +24,7 @@ predictions = MLP.predict(X_test)
 pred_labels = np.argmax(predictions, axis=1)
 true_labels = np.argmax(y_test, axis=1)
 
-for pred, target in zip(pred_labels, true_labels):
-    print(f'Predicted: {pred}     Ground truth: {target}')
+for pred, target, probs in zip(pred_labels, true_labels, predictions):
+    percentages = [f"{p*100:.2f}%" for p in probs]  
+    confidence = probs[pred] * 100  
+    print(f'Predicted: {pred} ({confidence:.2f}%)     Ground truth: {target}     Probabilities: {percentages}')
